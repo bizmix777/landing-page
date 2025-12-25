623dc76cfc617ee71c41449fd6345e8dfd700069
bizmix777 <bizmix777@gmail.com>
2025-12-25 16:35:11 -0300

    Update vite.config.js

--- direito/vite.config.js
+++ direito/vite.config.js
@@ -1,19 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // SEM A LINHA 'base'
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  plugins: [react()],
+  base: '/direito/',  // <-- ADICIONE ESSA LINHA para a subpasta
+  build: {
+    outDir: 'dist'  // Pasta de saída do build
+  }
+})
